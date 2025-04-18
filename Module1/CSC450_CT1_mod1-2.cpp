9317894c72ba97f40af0265f9f0573606b0f8e3a
JaredSargent <jared.sargent@csuglobal.edu>
2025-04-17 22:11:20 -0400

    overlooked error

--- Module1/CSC450_CT1_mod1-2.cpp
+++ Module1/CSC450_CT1_mod1-2.cpp
@@ -8,7 +8,6 @@ int main() {
     double myMoney = 1000.50; // Amount to be printed out
 
     // Standard Output Statements
-    cout << "Please be sure to correct all syntax errors in this program" << endl;
     cout << "I have corrected all errors for this program." << endl;
     cout << "The total amount of money available is = " << myMoney << endl;
 
