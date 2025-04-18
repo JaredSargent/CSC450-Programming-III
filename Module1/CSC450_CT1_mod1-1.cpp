96a61527f92ba7aa344f6cfb935ea4b25f9a8da9
JaredSargent <jared.sargent@csuglobal.edu>
2025-04-17 21:41:42 -0400

    Initial

---
+++ Module1/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,13 @@
+// CSC450_CT1_mod1-1.cpp
+// Corrected program to display welcome messages
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Standard Output Statements
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    return 0;
+}
---
+++ Module1/CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,17 @@
+
+// CSC450_CT1_mod1-2.cpp
+// Corrected program to display money amount
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double myMoney = 1000.50; // Amount to be printed out
+
+    // Standard Output Statements
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    return 0;
+}
